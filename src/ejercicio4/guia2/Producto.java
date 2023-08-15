9224a719fcc787a7236c4e57d2f7d3b76a9118ea
tamaracantero <141643686+tamaracantero@users.noreply.github.com>
2023-08-15 12:58:58 -0300

    clase producto y jframe inicial
    se crean la clase Producto y la ventana MenuDeTodoSA

---
+++ src/ejercicio4/guia2/Producto.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4.guia2;
+
+/**
+ *
+ * @author valkiria
+ */
+public class Producto implements Comparable<Producto>{
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String categoria;
+
+    public Producto(int codigo, String descripcion, double precio, int stock, String categoria) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int compareTo(Producto o) {
+        if(codigo==o.codigo){
+            return 0;
+        }else if(codigo>o.codigo){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    
+}
